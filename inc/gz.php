182f49bf63e7fe012f99cd890a9ea9a3ee6f61fb
Marko Heijnen <info@markoheijnen.com>
2016-02-05 01:15:13 +0100

    Add GZ class. See #4

---
+++ inc/gz.php
@@ -0,0 +1,45 @@
+<?php
+
+class Minit_Pro_GZ {
+
+	public static function compress( $source, $data, $level = 9 ) {
+		$mode  = 'wb' . $level;
+		$fp    = gzopen( $source . '.gz', $mode );
+
+		gzwrite( $fp, $data );
+		
+		return gzclose( $fp );
+	}
+
+	public static function compress_source( $source, $level = 9 ) {
+		$dest  = $source . '.gz';
+		$mode  = 'wb' . $level;
+		$error = false;
+
+		if ( $fp_out = gzopen( $dest, $mode ) ) {
+			if ( $fp_in = fopen( $source, 'rb' ) ) {
+				while ( !feof($fp_in) ) {
+					gzwrite( $fp_out, fread( $fp_in, 1024 * 512 ) );
+				}
+
+				fclose( $fp_in );
+			}
+			else {
+				$error = true;
+			}
+
+			gzclose( $fp_out );
+		}
+		else {
+			$error = true;
+		}
+
+		if ( $error ) {
+			return false;
+		}
+		else {
+			return $dest;
+		}
+	} 
+
+}
